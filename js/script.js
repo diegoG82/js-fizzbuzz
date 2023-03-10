e7487f5e335830dd264c1908ac38162940a6d380
diegoG82 <diegogiardina@hotmail.it>
2023-03-10 15:27:45 +0100

    milestone 1 complete

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
-alert("ciao!")
+// Scrivi un programma che stampi in console i numeri da 1 a 100,
+//  ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
+//  Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+// :alphabet-yellow-question:**Prima di partire a scrivere codice poniamoci qualche domanda:**
+// Come faccio a sapere se un numero è divisibile per un altro?
+// Abbiamo visto qualcosa di particolare che possiamo usare?
+
+// :lampadina:**Consigli del giorno:**
+//  1. Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+//  2. Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+
+// **BONUS 1:**
+//  Crea un container nel DOM , aggiungendo un elemento html con il numero o la stringa corretta da mostrare.
+//  **BONUS 2:**
+//  Applica stili differenti agli elementi aggiunti al DOM nel *BONUS 1*, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
+
+
+// MILESTONE 1
+
+
+for (let i = 1; i <= 100; i++){
+    console.log (i);
+
+    if (i % 15 === 0){
+        console.log("fizzbuzz");
+    }
+
+    else if (i % 5 === 0){
+        console.log("Buzz")
+    }
+
+    else if (i % 3 === 0)
+    console.log ("Fizz")
+}
