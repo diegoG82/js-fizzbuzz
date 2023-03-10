6c50250f97b86030a79c91ef3ab7696240b9dc21
diegoG82 <diegogiardina@hotmail.it>
2023-03-10 16:59:33 +0100

    Inserita bozza bonus da abbellire.

--- js/script.js
+++ js/script.js
@@ -15,25 +15,57 @@
 //  **BONUS 2:**
 //  Applica stili differenti agli elementi aggiunti al DOM nel *BONUS 1*, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
 
+
 // MILESTONE 1
 
+// let fizzgame;
 
-let fizzgame;
+// for (let i = 1; i <= 100; i++) {
+
+//   if (i % 15 === 0) {
+//     fizzgame="fizzbuzz";
+
+//   } else if (i % 5 === 0) {
+//     fizzgame="buzz";
+
+//   } else if (i % 3 === 0){
+//     fizzgame="fizz";
+
+//   } else {
+//     fizzgame = i;   
+//   }
+//     console.log(fizzgame);
+// }
+
+
+
+// BONUS
+
+let typeOfBox;
+let coloRofBox;
+
+const fizzgame = document.querySelector(".row")
 
 for (let i = 1; i <= 100; i++) {
 
   if (i % 15 === 0) {
-    fizzgame="fizzbuzz";
+    typeOfBox = "FIZZBUZZ";
+    coloRofBox = "red";
 
   } else if (i % 5 === 0) {
-    fizzgame="buzz";
+    typeOfBox = "BUZZ";
+    coloRofBox = "yellow";
 
   } else if (i % 3 === 0){
-    fizzgame="fizz";
+    typeOfBox = "FIZZ";
+    coloRofBox = "green";
 
   } else {
-    fizzgame = i;   
+    typeOfBox = i
+    coloRofBox = "blue";   
   }
-    console.log(fizzgame);
+    console.log(i, typeOfBox, coloRofBox);  
+   fizzgame.innerHTML += `<div class="col${coloRofBox}"><span>${typeOfBox}</span></div>`;
 }
+
 
